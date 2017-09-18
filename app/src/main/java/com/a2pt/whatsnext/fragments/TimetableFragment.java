1632f77ba7128efb8ca3275355f9ff3156e174e8
Carl Meyer <carlmeyer1996@gmail.com>
2017-09-18 17:19:24 +0200

    Commit #C24 - TimeTable Alignment Fixed
    •  Adjusted the Activity Adapter Layout so that everything is aligned neatly underneath each other.

--- app/src/main/java/com/a2pt/whatsnext/fragments/TimetableFragment.java
+++ app/src/main/java/com/a2pt/whatsnext/fragments/TimetableFragment.java
@@ -60,52 +60,52 @@ public class TimetableFragment extends Fragment {
         monday.add(new Activity("MATH203", Activity.Activity_Type.LECTURE, "35 00 17", new LocalTime(7,45)));
         monday.add(new Activity("WRAP302", Activity.Activity_Type.LECTURE, "35 01 01", new LocalTime(9,5)));
 
+      /*  monday.add(new Activity("WRAP302", Activity.Activity_Type.LECTURE, "35 01 01", new LocalTime(9,5)));
         monday.add(new Activity("WRAP302", Activity.Activity_Type.LECTURE, "35 01 01", new LocalTime(9,5)));
         monday.add(new Activity("WRAP302", Activity.Activity_Type.LECTURE, "35 01 01", new LocalTime(9,5)));
         monday.add(new Activity("WRAP302", Activity.Activity_Type.LECTURE, "35 01 01", new LocalTime(9,5)));
         monday.add(new Activity("WRAP302", Activity.Activity_Type.LECTURE, "35 01 01", new LocalTime(9,5)));
         monday.add(new Activity("WRAP302", Activity.Activity_Type.LECTURE, "35 01 01", new LocalTime(9,5)));
-        monday.add(new Activity("WRAP302", Activity.Activity_Type.LECTURE, "35 01 01", new LocalTime(9,5)));
-
+*/
         tuesday.add(new Activity("MATH214", Activity.Activity_Type.LECTURE, "35 00 17", new LocalTime(7,45)));
         tuesday.add(new Activity("MATH214 Tut", Activity.Activity_Type.LECTURE, "04 00 01", new LocalTime(9,5)));
         tuesday.add(new Activity("STAT203", Activity.Activity_Type.LECTURE, "04 00 3", new LocalTime(14,5)));
         tuesday.add(new Activity("STAT203 Prac", Activity.Activity_Type.LECTURE, "07 02 48", new LocalTime(15,30)));
         tuesday.add(new Activity("STAT203 Tut", Activity.Activity_Type.LECTURE, "07 02 48", new LocalTime(16,45)));
-
+/*
         tuesday.add(new Activity("STAT203 Tut", Activity.Activity_Type.LECTURE, "07 02 48", new LocalTime(16,45)));
         tuesday.add(new Activity("STAT203 Tut", Activity.Activity_Type.LECTURE, "07 02 48", new LocalTime(16,45)));
         tuesday.add(new Activity("STAT203 Tut", Activity.Activity_Type.LECTURE, "07 02 48", new LocalTime(16,45)));
-
+*/
 
         wednesday.add(new Activity("STAT203", Activity.Activity_Type.LECTURE, "35 00 17", new LocalTime(7,45)));
         wednesday.add(new Activity("MATH203", Activity.Activity_Type.LECTURE, "35 00 18", new LocalTime(9,5)));
         wednesday.add(new Activity("WRR301", Activity.Activity_Type.LECTURE, "09 02 02", new LocalTime(10,25)));
         wednesday.add(new Activity("MATH214", Activity.Activity_Type.LECTURE, "07 02 50", new LocalTime(14,5)));
         wednesday.add(new Activity("WRAP302 Prac", Activity.Activity_Type.LECTURE, "09 02 04", new LocalTime(15,35)));
-
+/*
         wednesday.add(new Activity("WRAP302 Prac", Activity.Activity_Type.LECTURE, "09 02 04", new LocalTime(15,35)));
         wednesday.add(new Activity("WRAP302 Prac", Activity.Activity_Type.LECTURE, "09 02 04", new LocalTime(15,35)));
         wednesday.add(new Activity("WRAP302 Prac", Activity.Activity_Type.LECTURE, "09 02 04", new LocalTime(15,35)));
-
+*/
         thursday.add(new Activity("MATH214", Activity.Activity_Type.LECTURE, "07 02 50", new LocalTime(7,45)));
         thursday.add(new Activity("WRL301", Activity.Activity_Type.LECTURE, "35 00 18", new LocalTime(10,25)));
         thursday.add(new Activity("WRL301 Tut", Activity.Activity_Type.LECTURE, "35 00 16", new LocalTime(14,5)));
         thursday.add(new Activity("STAT203", Activity.Activity_Type.LECTURE, "35 00 18", new LocalTime(16,45)));
 
-
+/*
         thursday.add(new Activity("STAT203", Activity.Activity_Type.LECTURE, "35 00 18", new LocalTime(16,45)));
         thursday.add(new Activity("STAT203", Activity.Activity_Type.LECTURE, "35 00 18", new LocalTime(16,45)));
         thursday.add(new Activity("STAT203", Activity.Activity_Type.LECTURE, "35 00 18", new LocalTime(16,45)));
         thursday.add(new Activity("STAT203", Activity.Activity_Type.LECTURE, "35 00 18", new LocalTime(16,45)));
-
+*/
 
 
         //no classes on monday so I will add an empty activity just so that the array isn't null
         //TODO: cater for when there are no classes on a day
 
         friday.add(new Activity("WRMS302", Activity.Activity_Type.LECTURE, "09 02 02", new LocalTime(9,5)));
-
+/*
         friday.add(new Activity("", Activity.Activity_Type.LECTURE, "", new LocalTime(0,0)));
         friday.add(new Activity("", Activity.Activity_Type.LECTURE, "", new LocalTime(0,0)));
         friday.add(new Activity("", Activity.Activity_Type.LECTURE, "", new LocalTime(0,0)));
@@ -113,7 +113,7 @@ public class TimetableFragment extends Fragment {
         friday.add(new Activity("", Activity.Activity_Type.LECTURE, "", new LocalTime(0,0)));
         friday.add(new Activity("", Activity.Activity_Type.LECTURE, "", new LocalTime(0,0)));
         friday.add(new Activity("", Activity.Activity_Type.LECTURE, "", new LocalTime(0,0)));
-
+*/
         //set adapters
         monAdapter = new ActivityAdapter(getActivity(), monday);
         tueAdapter = new ActivityAdapter(getActivity(), tuesday);
@@ -151,7 +151,7 @@ public class TimetableFragment extends Fragment {
             case 2:
             case 3:
             case 4: setMinusAmount(lvWednesday,320);break;
-            case 5:
+            case 5: setMinusAmount(lvWednesday,360);break;
             case 6: setMinusAmount(lvWednesday, 380);break;
             case 7: setMinusAmount(lvWednesday, 400);break;
             case 8: setMinusAmount(lvWednesday,420);break;
